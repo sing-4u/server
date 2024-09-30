9d8f7938e15e40350934afe1907d1b63d7b6ede5
leemhoon00 <leemhoon000@gmail.com>
2024-09-30 16:47:06 +0900

    set ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      expandVariables: true,
+      cache: true,
+    }),
+  ],
 })
 export class AppModule {}
