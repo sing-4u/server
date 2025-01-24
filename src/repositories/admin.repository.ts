9679b3d8fa09b630d9d1dd85124c4782547f94d1
leemhoon00 <leemhoon000@gmail.com>
2025-01-24 17:42:45 +0900

    feat: 관리자 페이지 아티스트 조회

--- src/repositories/admin.repository.ts
+++ src/repositories/admin.repository.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class AdminRepository {
@@ -53,6 +54,120 @@ export class AdminRepository {
     await this.prisma.admin.update({
       where: { id },
       data: input,
+    });
+  }
+
+  // TO REMOVE
+  async getArtists({
+    index,
+    sort,
+    search,
+  }: {
+    index: number;
+    sort: 'latest' | 'isOpen' | 'songListCount';
+    search?: string;
+  }) {
+    let orderBy: Prisma.UserOrderByWithRelationInput;
+    if (sort === 'latest') {
+      orderBy = {
+        createdAt: 'desc',
+      };
+    } else if (sort === 'isOpen') {
+      orderBy = {
+        isOpened: 'desc',
+      };
+    } else {
+      orderBy = {
+        SongList: {
+          _count: 'desc',
+        },
+      };
+    }
+
+    let where: Prisma.UserWhereInput = {};
+    if (search) {
+      where = {
+        name: {
+          contains: search,
+        },
+      };
+    }
+    return await this.prisma.user.findMany({
+      select: {
+        id: true,
+        email: true,
+        name: true,
+        image: true,
+        createdAt: true,
+        isOpened: true,
+        _count: {
+          select: {
+            SongList: true,
+          },
+        },
+      },
+      orderBy,
+      skip: index * 10,
+      take: 10,
+      where,
+    });
+  }
+
+  async getArtist(id: string) {
+    return await this.prisma.user.findUniqueOrThrow({
+      where: { id },
+      select: {
+        id: true,
+        email: true,
+        name: true,
+        provider: true,
+      },
+    });
+  }
+
+  async getRequest(email: string) {
+    return await this.prisma.song.findMany({
+      where: {
+        email,
+      },
+      select: {
+        artist: true,
+        title: true,
+        createdAt: true,
+        songList: {
+          select: {
+            user: {
+              select: {
+                name: true,
+              },
+            },
+          },
+        },
+      },
+    });
+  }
+
+  async getSongList(id: string) {
+    return await this.prisma.songList.findMany({
+      where: {
+        userId: id,
+      },
+      select: {
+        id: true,
+        startDate: true,
+        endDate: true,
+        songs: {
+          select: {
+            artist: true,
+            title: true,
+            createdAt: true,
+            email: true,
+          },
+        },
+      },
+      orderBy: {
+        startDate: 'desc',
+      },
     });
   }
 }
